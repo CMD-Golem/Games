b9a88097cff766812493a6e9337d5159d88f05c9
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-04-18 17:17:58 +0200

    PWA support

---
+++ sw.js
@@ -0,0 +1,48 @@
+self.addEventListener("install", e => {
+	e.waitUntil(
+		caches.open("static").then(cache => {
+			return cache.addAll(
+				[
+					"./",
+					"./settings.html",
+					"./elements/javascript/sheep-game.js",
+					"./elements/javascript/sheep-game-settings.js",
+					"./elements/javascript/sheep-game-var.js",
+
+					"./elements/pictures/sheeps/black_sheep.png",
+					"./elements/pictures/sheeps/blue_sheep.png",
+					"./elements/pictures/sheeps/brown_sheep.png",
+					"./elements/pictures/sheeps/gray_sheep.png",
+					"./elements/pictures/sheeps/green_sheep.png",
+					"./elements/pictures/sheeps/purple_sheep.png",
+					"./elements/pictures/sheeps/red_sheep.png",
+					"./elements/pictures/sheeps/white_sheep.png",
+
+					"./elements/pictures/clover.svg",
+					"./elements/pictures/confirm.svg",
+					"./elements/pictures/dice.svg",
+					"./elements/pictures/goal.svg",
+					"./elements/pictures/goal_bg.svg",
+					"./elements/pictures/instruction.svg",
+					"./elements/pictures/medal.svg",
+					"./elements/pictures/path.svg",
+					"./elements/pictures/restart.svg",
+					"./elements/pictures/settings.svg",
+					"./elements/pictures/wolf.svg",
+					
+					"./elements/style/sheep-game.css",
+					"./elements/style/sheep-game-settings.css",
+					"./elements/style/style.css"
+				]
+			);
+		})
+	);
+});
+
+self.addEventListener("fetch", e => {
+	e.respondWith(
+		caches.matches(e.request).then(response => {
+			return response || fetch(e.request);
+		})
+	);
+});
